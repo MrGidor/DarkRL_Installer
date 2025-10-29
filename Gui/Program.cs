298965c8cedd43531b506bb57d9c6e86e27a2358
Gidor <gidorstudios.help@gmail.com>
2025-10-29 11:08:29 +0100

    feat: Initial Avalonia setup

---
+++ Gui/Program.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Modpack_Installer.Gui;
+using System;
+
+namespace Modpack_Installer.Gui;
+
+sealed class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect() // Make sure you have 'using Avalonia;' at the top and reference Avalonia.Desktop
+            .WithInterFont()
+            .LogToTrace();
+}
+
---
+++ Gui/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Modpack_Installer.Gui.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
