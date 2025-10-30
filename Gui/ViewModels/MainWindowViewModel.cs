c06f49d3907c21ae0d34fb56df4826778243478c
Gidor <gidorstudios.help@gmail.com>
2025-10-30 16:49:12 +0100

    Revert "fix(curl-dw): pass the correct variable to FinishDownload"
    This reverts commit df2af0391acc277e16b51d4113a8d74f5feb98d1.

--- Gui/ViewModels/MainWindowViewModel.cs
+++ Gui/ViewModels/MainWindowViewModel.cs
@@ -112,7 +112,7 @@ public partial class MainWindowViewModel : ObservableObject
 
                 if (proc.ExitCode == 0)
                 {
-                    FinishDownload(modPath);
+                    FinishDownload(path);
 
                     return;
                 }
